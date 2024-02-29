f6384e37fa6830e78c12f7a4f7dbe07a85e1c8ad
johnny-woodtke <johnnywoodtke@gmail.com>
2024-02-29 09:52:49 -0500

    use shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
-import "styles/tailwind.css"
+import "@/styles/global.css"
+import { Inter as FontSans } from "next/font/google"
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen bg-background font-sans antialiased">{children}</body>
     </html>
   )
 }
