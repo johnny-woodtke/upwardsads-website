580097cdd5eb3e392fdc986a718a6faefae5e838
johnny-woodtke <johnnywoodtke@gmail.com>
2024-03-09 02:08:50 -0500

    add packages and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { NavigationMenu } from "@/components/NavigationMenu"
 import "@/styles/global.css"
 
@@ -7,6 +9,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="min-h-screen bg-black/95 bg-gradient-to-tr from-background">
         <NavigationMenu />
         <div className="flex w-full flex-col items-center">{children}</div>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
