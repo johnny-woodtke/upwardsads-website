f71241114aa598859c3f80f149410e677a09884a
johnny-woodtke <johnnywoodtke@gmail.com>
2024-03-20 13:04:30 -0400

    middleware rules

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,21 @@ import type { NextRequest } from "next/server"
 
 export function middleware(req: NextRequest) {
   try {
-    isValidOriginOrThrow(req)
-  } catch (e) {
-    return NextResponse.error()
+    const pathname = new URL(req.url).pathname
+    for (const rule of Object.keys(ruleHandlers) as Rule[]) {
+      if (isPathExemptFromRule(pathname, rule)) {
+        continue
+      }
+      ruleHandlers[rule](req)
+    }
+    return NextResponse.next()
+  } catch (e: any) {
+    const ruleError = ruleErrors[e.message as Rule]
+    if (ruleError) {
+      return NextResponse.json({ status: "error", message: ruleError.message }, { status: ruleError.status })
+    }
+    return NextResponse.json({ status: "error", message: e.message }, { status: 500 })
   }
-  return NextResponse.next()
 }
 
 export const config = {
@@ -15,10 +25,47 @@ export const config = {
   matcher: ["/api/:path*"],
 }
 
-function isValidOriginOrThrow(req: NextRequest) {
-  const { origin: targetOrigin } = new URL(req.url)
-  const requestOrigin = req.headers.get("origin")
-  if (targetOrigin !== requestOrigin) {
-    throw new Error("Forbidden")
+type Rule = "same-origin"
+
+const pathConfigs: {
+  pathname: string
+  exemptions: Rule[]
+}[] = [
+  {
+    pathname: "/api/health",
+    exemptions: ["same-origin"],
+  },
+]
+
+function isPathExemptFromRule(pathname: string, rule: Rule) {
+  return pathConfigs.some((config) => config.pathname === pathname && config.exemptions.includes(rule))
+}
+
+class RuleError extends Error {
+  constructor(public rule: Rule) {
+    super(rule)
+  }
+}
+
+const ruleErrors: Record<
+  Rule,
+  {
+    message: string
+    status: number
+  }
+> = {
+  "same-origin": {
+    message: "Request origin is not allowed.",
+    status: 403,
+  },
+}
+
+const ruleHandlers: Record<Rule, (req: NextRequest) => void> = {
+  "same-origin": isSameOriginOrThrow,
+}
+
+function isSameOriginOrThrow(req: NextRequest) {
+  if (new URL(req.url).origin !== req.headers.get("origin")) {
+    throw new RuleError("same-origin")
   }
 }
