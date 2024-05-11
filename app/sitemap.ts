1c62c2e700baee48a0d7a3308c39d2fbcfa9bc28
johnny-woodtke <johnnywoodtke@gmail.com>
2024-05-11 12:18:12 -0400

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://upwardsads.com",
+      lastModified: new Date(),
+      priority: 1,
+    },
+    {
+      url: "https://upwardsads.com/contact",
+      lastModified: new Date(),
+      priority: 0.9,
+    },
+    {
+      url: "https://upwardsads.com/assets/icon.png",
+      lastModified: new Date(),
+      priority: 0.5,
+    },
+  ]
+}
