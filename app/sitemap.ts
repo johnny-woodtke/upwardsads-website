54aae1355df54e8dd1edbb51b473bbd2052e531c
johnny-woodtke <johnnywoodtke@gmail.com>
2024-05-11 13:19:21 -0400

    no lastModified

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,17 +4,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://upwardsads.com",
-      lastModified: new Date(),
       priority: 1,
     },
     {
       url: "https://upwardsads.com/contact",
-      lastModified: new Date(),
       priority: 0.9,
     },
     {
       url: "https://upwardsads.com/icon.png",
-      lastModified: new Date(),
       priority: 0.5,
     },
   ]
