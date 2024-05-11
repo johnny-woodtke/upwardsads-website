36c84084c03b8d99b1b8932efdadecc699646329
johnny-woodtke <johnnywoodtke@gmail.com>
2024-05-11 14:33:13 -0400

    rm useless metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,22 +23,16 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: TITLE,
-    description: DESCRIPTION,
-    images: [
-      {
-        url: "/icon-og.png",
-      },
-    ],
   },
   openGraph: {
     type: "website",
-    title: TITLE,
-    description: DESCRIPTION,
     url: "https://upwardsads.com",
     images: [
       {
+        width: 1200,
+        height: 630,
         url: "/icon-og.png",
+        type: "image/png",
       },
     ],
   },
